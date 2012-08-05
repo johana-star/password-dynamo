1536a7b71985ee379aefa3c0dc941f5cd0ad10b9
Strand McCutchen <strand@bettermistak.es>
2012-08-05 15:15:38 -0700

    0.00001 release of password-dynamo

---
+++ dynamo.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+
+get '/' do
+  words, password                                   = [], ''
+  min_word_length, max_word_length, words_in_password = 4, 8, 4 + rand(2)
+
+  File.open("words.txt") do |file|
+    file.each do |line| 
+      if line.length <= max_word_length and line.length >= min_word_length
+        words << line.strip.downcase
+      end
+    end
+  end
+
+  def spacer
+    array = Array(0..9) + ['!', '~', '-', '_', ' ', '+', '=', '%', '$', '#', '@']
+    array.shuffle.first.to_s
+  end
+
+  words_in_password.times do
+    password += words.shuffle.first + spacer
+  end
+
+  password.chop!
+
+  "#{password}"
+end
