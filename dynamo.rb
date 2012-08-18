242515052cd249758b6c19da2584ef3f44ade533
Strand McCutchen <strand@bettermistak.es>
2012-08-18 14:17:42 -0500

    Style and content revisions.

--- dynamo.rb
+++ dynamo.rb
@@ -1,5 +1,6 @@
 # Run on Heroku using Ruby 1.9.1
 require 'sinatra'
+require 'action_view'
 
 def load_dictionary(dictionary = "words.txt", max_word_length = 10)
   words = []
@@ -10,21 +11,20 @@ def load_dictionary(dictionary = "words.txt", max_word_length = 10)
     end
   end
   
-  return words
+  words
 end
 
-SPACERS = Array(0..9).collect {|i| i.to_s} + ['!', '~', '-', '_', ' ', '+', '=', '%', '$', '#', '@']
-WORDS   = load_dictionary
+WORDS = load_dictionary
 
 def generate_password
   password          = ''
-  words_in_password = 3 + rand(2)
+  @words_in_password = 4
 
-  (words_in_password - 1).times do
-    password += WORDS.sample + SPACERS.sample
+  (@words_in_password - 1).times do
+    password += WORDS.sample + ' ' 
   end
 
-  password += WORDS.sample
+  password + WORDS.sample
 end
 # FIXME Everything above this line can probably be stored in a class.
 
@@ -33,7 +33,10 @@ before do
 end
 
 get '/' do
-  @password = generate_password
+  @password       = generate_password
+  @words_count   = WORDS.count
+  @helper         = Object.new.extend(ActionView::Helpers::NumberHelper)
+  @length         = @password.length
 
   erb :index
 end
@@ -48,7 +51,68 @@ __END__
   <head>
     <title><%= @title %></title>
     <style>
-      body { margin: 40px; font: 40px/48px helvetica;}
+      body {
+        background-color: #FAFAFA;
+        margin:       38px;
+      }
+
+      p   {
+        padding:        0;
+        margin:         0;
+        font-family:    "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+      }
+
+      a {
+        color:            #33C;
+        text-decoration:  none;
+      }
+
+       a:visited {
+        color:            #339;
+      }
+      
+      hr {
+        width:            50%;
+        height:           1px;
+        color:            #999;
+        background-color: #999;
+        border:           none;
+      }
+
+      .one   {
+        font-size:    76px;
+        line-height:  114px;
+        font-weight:  100;
+        color:        #111;
+      }
+
+      .two   {
+        font-size:    47px;
+        line-height:  70px;
+        font-weight:  300;
+        color:        #333;
+      }
+
+      .three {
+        font-size:    29px;
+        line-height:  44px;
+        font-weight:  500;
+        color:        #555;
+      }
+
+      .four  {
+        font-size:    18px;
+        line-height:  27px;
+        font-weight:  700;
+        color:        #777;
+      }
+
+      .five {
+        font-size:    11px;
+        line-height:  17px;
+        font-weight:  900;
+        color:        #999;
+      }
     </style>
   </head>
   <body>
@@ -57,4 +121,11 @@ __END__
 </html>
 
 @@ index
-<p><%= @password %></p>
+<% possible_phrases = @words_count**@words_in_password %>
+<p class="five">Your new password is:</p>
+<p class="one"><%= @password %></p>
+<p class="two">There are <%= @words_count %> words in the dictionary.</p>
+<p class="three">Thus, there are <%= @helper.number_to_human possible_phrases %> possible passphrases. <span class="four">In security parlance there are <%= possible_phrases.to_s(2).length %> bits of entropy.*</span></p>
+<p class="two">So it's probably better than <a href="http://xkcd.com/936/">what you're currently using</a>.</p>
+<hr/>
+<p class="five">*These bits of entropy are based off of words in the dictionary, not characters in the string. If someone is brute forcing your password with just characters, it will be a lot harder for them.</p>
